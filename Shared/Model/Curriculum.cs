8d01ac1759ab4858e885b4fda94ce3c6dcd0d0ad
Enore, Kristian Paul Dizon <61934507+Krisore@users.noreply.github.com>
2023-02-13 02:34:57 +0800

    azure sql

--- Shared/Model/Curriculum.cs
+++ Shared/Model/Curriculum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -14,5 +15,26 @@ namespace HIVE.Shared.Model
         public string Alt { get; set; } = string.Empty;
         private List<Document> Documents { get; set; } = new List<Document>();
         public bool ShowDocuments;
+        public Color Color = Color.Beige;
+
+        public Curriculum()
+        {
+            if (Alt.Equals("BSIT"))
+            {
+                Color = Color.Blue;
+            }
+            else if(Alt.Equals("DCIT"))
+            {
+                Color = Color.SteelBlue;
+            }
+            else if (Alt.Equals("BSCPE"))
+            {
+                Color = Color.SlateGray;
+            }
+            else if (Alt.Equals("BSCPE"))
+            {
+                Color = Color.SlateGray;
+            }
+        }
     }
 }
